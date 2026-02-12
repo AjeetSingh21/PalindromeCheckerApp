e38724848b477c7dd9ecd9007486a289773cbb2f
Ajeet Singh <ajeet.singh77@outlook.com>
2026-02-12 16:22:35 +0530

    UC1: Application Entry & Welcome Message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,10 @@
+/*
+USE CASE 1: Palindrome Checker App
+Author: Ajeet Singh
+Date : 12-02-26
+Reg No. RA2411026010293
+*/
+
 public class PalindromeCheckerApp
 {
     public static void main(String[] args) {

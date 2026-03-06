508abd6627f9b9e452f06d10d8f9a4d233faf16d
AjeetSingh21 <ajeet.singh77@otlook.com>
2026-03-06 15:05:02 +0530

    Ajeet Singh Added UC13

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,65 +1,66 @@
-
 import java.util.*;
 
-interface PalindromeStrategy {
-    boolean checkPalindrome(String input);
-}
+public class PalindromeCheckerApp {
 
-class StackStrategy implements PalindromeStrategy {
-    public boolean checkPalindrome(String input) {
+    public static boolean checkWithTwoPointer(String input) {
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        int start = 0, end = normalized.length() - 1;
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static boolean checkWithStack(String input) {
         String normalized = input.replaceAll("\\s+", "").toLowerCase();
         Stack<Character> stack = new Stack<>();
+        for (char c : normalized.toCharArray()) stack.push(c);
         for (char c : normalized.toCharArray()) {
-            stack.push(c);
-        }
-        for (char c : normalized.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
+            if (c != stack.pop()) return false;
         }
         return true;
     }
-}
-class DequeStrategy implements PalindromeStrategy {
-    public boolean checkPalindrome(String input) {
+
+    public static boolean checkWithDeque(String input) {
         String normalized = input.replaceAll("\\s+", "").toLowerCase();
         Deque<Character> deque = new LinkedList<>();
-        for (char c : normalized.toCharArray()) {
-            deque.add(c);
-        }
+        for (char c : normalized.toCharArray()) deque.add(c);
         while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
-                return false;
-            }
+            if (deque.removeFirst() != deque.removeLast()) return false;
         }
         return true;
     }
-}
 
-public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("=== UC13: Performance Comparison of Palindrome Algorithms ===");
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        System.out.println("Choose strategy: 1 for Stack, 2 for Deque");
-        int choice = scanner.nextInt();
+        long startTime, endTime;
 
-        PalindromeStrategy strategy;
-        if (choice == 1) {
-            strategy = new StackStrategy();
-        } else {
-            strategy = new DequeStrategy();
-        }
+        startTime = System.nanoTime();
+        boolean twoPointerResult = checkWithTwoPointer(input);
+        endTime = System.nanoTime();
+        long twoPointerTime = endTime - startTime;
 
-        boolean result = strategy.checkPalindrome(input);
+        startTime = System.nanoTime();
+        boolean stackResult = checkWithStack(input);
+        endTime = System.nanoTime();
+        long stackTime = endTime - startTime;
 
-        if (result) {
-            System.out.println("The string \"" + input + "\" is a palindrome.");
-        } else {
-            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
-        }
+        startTime = System.nanoTime();
+        boolean dequeResult = checkWithDeque(input);
+        endTime = System.nanoTime();
+        long dequeTime = endTime - startTime;
+
+        System.out.println("\nResults:");
+        System.out.println("Two-Pointer: " + twoPointerResult + " | Time: " + twoPointerTime + " ns");
+        System.out.println("Stack:       " + stackResult + " | Time: " + stackTime + " ns");
+        System.out.println("Deque:       " + dequeResult + " | Time: " + dequeTime + " ns");
 
         scanner.close();
     }

e6aed8f9956dbb64caca6c4e6414ebd7f966d6b3
AjeetSingh21 <ajeet.singh77@otlook.com>
2026-03-06 14:39:27 +0530

    Ajeet Singh Added UC9

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,69 +1,33 @@
-// File: UseCase8PalindromeCheckerApp.java
+// File: UseCase9PalindromeCheckerApp.java
 
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    static class Node {
-        char data;
-        Node next;
-        Node(char data) {
-            this.data = data;
+    public static boolean isPalindrome(String input, int start, int end) {
+        if (start >= end) {
+            return true;
         }
+        if (input.charAt(start) != input.charAt(end)) {
+            return false;
+        }
+        return isPalindrome(input, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
-        Node head = null, tail = null;
-        for (char c : input.toCharArray()) {
-            Node node = new Node(c);
-            if (head == null) {
-                head = node;
-                tail = node;
-            } else {
-                tail.next = node;
-                tail = node;
-            }
-        }
 
-        Node slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
+        boolean result = isPalindrome(input, 0, input.length() - 1);
 
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-        boolean isPalindrome = true;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                isPalindrome = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        if (isPalindrome) {
+        if (result) {
             System.out.println("The string \"" + input + "\" is a palindrome.");
         } else {
             System.out.println("The string \"" + input + "\" is NOT a palindrome.");
         }
 
         scanner.close();
-    }
-
-    private static Node reverse(Node head) {
-        Node prev = null, current = head;
-        while (current != null) {
-            Node next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        return prev;
     }
 }
